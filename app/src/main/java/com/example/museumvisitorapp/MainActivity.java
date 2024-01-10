c6cffcea3c10aedd287aac0cb3d41b05f36273fe
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-11 01:40:36 +0800

    Change size of app logo

--- app/src/main/java/com/example/museumvisitorapp/MainActivity.java
+++ app/src/main/java/com/example/museumvisitorapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.museumvisitorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Drawable originalDrawable = getDrawable(R.drawable.ic_app_logo);
+        Bitmap originalBitmap = ((BitmapDrawable) originalDrawable).getBitmap();
+
+        // Create a scaled version of the Bitmap
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(
+                originalBitmap,
+                100,
+                100,
+                false);
+
+        // Convert the scaled Bitmap back to a Drawable
+        Drawable scaledDrawable = new BitmapDrawable(getResources(), scaledBitmap);
+
+        // Set the scaled logo as the action bar icon
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.drawable.ic_app_logo);
+        getSupportActionBar().setIcon(scaledDrawable);
 
     }
 
