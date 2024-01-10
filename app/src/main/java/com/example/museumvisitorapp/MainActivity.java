ef2f621ab3a2e495315a2a8dad2114664f9d8c07
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-11 01:29:55 +0800

    Add app logo in action bar

--- app/src/main/java/com/example/museumvisitorapp/MainActivity.java
+++ app/src/main/java/com/example/museumvisitorapp/MainActivity.java
@@ -8,7 +8,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_app_logo);
+
     }
+
 }
