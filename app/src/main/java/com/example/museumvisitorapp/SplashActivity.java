bb9fbd090ee9c4b186024ea5744c930c76afb1cd
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-11 01:07:06 +0800

    Apply scale animation in splash screen

--- app/src/main/java/com/example/museumvisitorapp/SplashActivity.java
+++ app/src/main/java/com/example/museumvisitorapp/SplashActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,6 +15,23 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ImageView logo = findViewById(R.id.logoImageView);
+
+        // Creating a scale animation to make the logo grow
+        ScaleAnimation scaleAnimation = new ScaleAnimation(
+                0.5f, 1f,
+                0.5f, 1f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f
+        );
+
+        // Set duration then keep the final state of the animation
+        scaleAnimation.setDuration(2000);
+        scaleAnimation.setFillAfter(true);
+
+        // Apply the scale animation to the ImageView
+        logo.startAnimation(scaleAnimation);
 
         new Handler().postDelayed(new Runnable() {
             @Override
