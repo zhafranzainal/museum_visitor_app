c7f2d0c009ecdef5c8ab16e0ee4736010731d292
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-11 10:42:57 +0800

    Customize screen title in the action bar

--- app/src/main/java/com/example/museumvisitorapp/GalleryActivity.java
+++ app/src/main/java/com/example/museumvisitorapp/GalleryActivity.java
@@ -1,15 +1,37 @@
 package com.example.museumvisitorapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class GalleryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Museum Levels & Galleries");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
     }
 
 }
